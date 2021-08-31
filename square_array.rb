91cd706062f80d810ca104047ed65fdd1f845140
BlakeMcD <blakemcd@gmail.com>
2021-09-01 09:53:41 +1000

    assignment passed

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  squared_numbers = []
+  array.each do |number|
+    squared_numbers << number**2
+  end
+  squared_numbers
+  # array.each do | number |
+  #   squared_numbers = "#{number}**2"
+  #   puts squared numbers
+  #   return squared_numbers
+  # end
 end
